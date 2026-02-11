a49560d7518afb1cf29f80e72327e8aeef35277d
GSNFonseka <nowanyafonseka@gmail.com>
2026-02-11 23:54:45 +0530

    feat: implement Signup page with creating Home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ const HomePage = () => {
   return (
     <main>
       <div>HomePage</div>
-      <Link href='./navigation-bar'>Login</Link>
-    </main>
+      <Link href='./login'>Login</Link><p></p>
+      <Link href="./signup">Sign Up</Link>
+    </main> 
     
   )
 }
