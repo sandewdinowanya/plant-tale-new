12751f5ac5d17f238fbbf1f53317840be44965bd
GSNFonseka <nowanyafonseka@gmail.com>
2026-02-11 23:59:21 +0530

    feat: implement navigation bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h1>MyApp</h1>
+            <div>
+                
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
